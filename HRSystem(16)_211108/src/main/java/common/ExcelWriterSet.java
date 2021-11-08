c6d53355c9670c92ebc623d0be02d16d2ac484f3
lyena8958 <89381986+lyena8958@users.noreply.github.com>
2021-11-08 23:54:25 +0900

    [HRSystem]_16
    작업(7)
    
    [잔여 작업]
    ㆍ퍼블리싱, 디자인 작업
    
    ● 그래프-데이터 오차보완
     ㆍ 타입별 데이터가 일치하지 않는 오류로 인해, 각각의 로직 수정완료
    
    ● 메인페이지 - 그래프 타입요청 유효성검사 처리
     ㆍ type : select
     ㆍ year : 1~12 정수 입력가능하도록 js처리 완료

---
+++ HRSystem(16)_211108/src/main/java/common/ExcelWriterSet.java
@@ -0,0 +1,116 @@
+package common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import controller.common.ExcelWriterCareer;
+import controller.common.ExcelWriterLicense;
+import controller.common.ExcelWriterMember;
+import controller.common.ExcelWriterSchool;
+
+@Component
+public class ExcelWriterSet {
+	
+	@Autowired
+	public ExcelWriterSet(ExcelWriterMember memberWriter, 
+			ExcelWriterCareer careerWriter, ExcelWriterSchool schoolWriter, ExcelWriterLicense licenseWriter){
+		System.out.println("ExcelWriterSet µµÂø");
+		this.memberWriter = memberWriter;
+		this.careerWriter = careerWriter;
+		this.schoolWriter = schoolWriter;
+		this.licenseWriter = licenseWriter;
+		System.out.println("?");
+		
+	}
+ 
+	private boolean career;
+	private boolean school;
+	private boolean	license;
+
+	private ExcelWriterMember memberWriter;
+	private ExcelWriterCareer careerWriter;
+	private ExcelWriterSchool schoolWriter;
+	private ExcelWriterLicense licenseWriter;
+	
+	
+	public boolean isCareer() {
+		return career;
+	}
+
+
+	public void setCareer(boolean career) {
+		this.career = career;
+	}
+
+
+	public boolean isSchool() {
+		return school;
+	}
+
+	
+	public void setSchool(boolean school) {
+		this.school = school;
+	}
+
+
+	public boolean isLicense() {
+		return license;
+	}
+
+	
+	public void setLicense(boolean license) {
+		this.license = license;
+	}
+
+
+	public ExcelWriterMember getMemberWriter() {
+		return memberWriter;
+	}
+
+
+	public void setMemberWriter(ExcelWriterMember memberWriter) {
+		this.memberWriter = memberWriter;
+	}
+
+
+	public ExcelWriterCareer getCareerWriter() {
+		return careerWriter;
+	}
+	
+
+	public void setCareerWriter(ExcelWriterCareer careerWriter) {
+		this.careerWriter = careerWriter;
+	}
+
+
+	public ExcelWriterSchool getSchoolWriter() {
+		return schoolWriter;
+	}
+
+	public void setSchoolWriter(ExcelWriterSchool schoolWriter) {
+		this.schoolWriter = schoolWriter;
+	}
+
+
+	public ExcelWriterLicense getLicenseWriter() {
+		return licenseWriter;
+	}
+
+
+	public void setLicenseWriter(ExcelWriterLicense licenseWriter) {
+		this.licenseWriter = licenseWriter;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ExcelWriterSet [career=" + career + ", school=" + school + ", license=" + license + ", memberWriter="
+				+ memberWriter + ", careerWriter=" + careerWriter + ", schoolWriter=" + schoolWriter
+				+ ", licenseWriter=" + licenseWriter + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ HRSystem(16)_211108/src/main/java/common/test.java
@@ -0,0 +1,30 @@
+package common;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		
+		String[] data;
+		
+		String[] s1 = {"2021", "1312", "31123", "23123"};
+		String[] s2 = {"2022", "1312", "31123", "23123"};
+		String[] s3 = {"2023", "1312", "31123", "23123"};
+		String[] s4 = {"2024", "1312", "31123", "23123"};
+		
+		data = s1;
+		data = s2;
+		data = s3;
+		data = s4;
+		
+		for(int i = 0; i < data.length; i++) {
+			System.out.println(data[i].);
+		}
+		
+		
+	}
+
+}
